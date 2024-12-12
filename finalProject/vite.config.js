e4186c164af59f808bd99233b1780d333fcb3c7f
hushbrush <harshitachakravadhanula@gmail.com>
2024-12-12 01:08:01 -0500

    changed

--- finalProject/vite.config.js
+++ finalProject/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/MajorStudioFinalProject/finalProject', // GitHub repository name
+  base: '/finalProject', // GitHub repository name
   build: {
     outDir: 'dist',  // Output directory
     rollupOptions: {
