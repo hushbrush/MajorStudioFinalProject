3fb55ac496da7171b3d48fa201142364713a7a9a
hushbrush <harshitachakravadhanula@gmail.com>
2024-12-26 20:23:33 +0530

    Use base as /finalProject/

--- finalProject/vite.config.js
+++ finalProject/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/MajorStudioFinalProject/finalProject/', // Full path for GitHub Pages
+  base: '/finalProject/', // Full path for GitHub Pages
   build: {
     outDir: 'dist',
     rollupOptions: {
