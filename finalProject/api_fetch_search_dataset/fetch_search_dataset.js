e86553cfe83e950a36aca1aabde83ec612d5c8a9
hushbrush <harshitachakravadhanula@gmail.com>
2025-02-03 14:01:23 -0500

    Update fetch_search_dataset.js

--- finalProject/api_fetch_search_dataset/fetch_search_dataset.js
+++ finalProject/api_fetch_search_dataset/fetch_search_dataset.js
@@ -2,7 +2,7 @@
 // check API documentation for search here: http://edan.si.edu/openaccess/apidocs/#api-search-search
 
 // put your API key here;
-const apiKey = "OBP6w8aw6IMti0Ip1efxd3z10OzeDyxqXVcRSCOK";
+const apiKey = "";
 
 // search base URL
 const searchBaseURL = "https://api.si.edu/openaccess/api/v1.0/search";
